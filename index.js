424c70c4b7c678225c42691d0a44ed7cf480f95f
tarunsingh070 <31713053+tarunsingh070@users.noreply.github.com>
2017-12-10 16:37:15 -0500

    Added the implementation for the following requests :
    1) POST add new Teller
    2) POST add new Client by teller
    3) POST add new transaction

--- index.js
+++ index.js
@@ -44,6 +44,78 @@ server
     // Maps req.body to req.params so there is no switching between them
     .use(restify.bodyParser())
 
+//POST add new Teller
+server.post('/api/tellers', function (req, res, next) {
+    var teller = new Tellers({
+        firstName: req.params.firstname,
+        lastName: req.params.lastname
+    });
+
+    teller.save(function (err) {
+        if (err) return handleError(err);
+
+        res.send(201, teller);
+    });
+    next();
+});
+
+//POST add new Client by teller
+server.post('/api/:teller_id/clients', function (req, res, next) {
+    Tellers.
+        findOne({ _id: req.params.teller_id}).
+        exec (function (err,tellers) {
+            var client1 = new Clients({
+                firstName: req.params.firstname,
+                lastName: req.params.lastname,
+                currentAddress: " ",
+                teller: req.params.teller_id
+            });
+
+            client1.save(function (err) {
+                if (err) {
+                    console.log(err);
+                    console.log(client1);
+                    console.log(tellers);
+                    return handleError(err);
+                }
+
+                res.send(201,client1);
+            });
+        });
+    next();
+
+});
+
+//POST add new transaction
+server.post('/api/clients/:client_id/transactions', function (req, res, next) {
+    //Clients.
+    //    findOne({ _id: req.params.client_id}).
+    //    exec (function (err, client) {
+    //        var transaction = new {
+    //            _id: new mongoose.Types.ObjectId(),
+    //            transactionName: req.params.transaction_name,
+    //            transactionDate: Date.now()
+    //        };
+    //        client.transactions.push(transaction);
+    //        client.save(function (err, newTransaction) {
+    //            if (err) return handleError(err);
+    //            res.send(201, newTransaction);
+    //        });
+    //    });
+
+    Clients.findOneAndUpdate({_id: req.params.client_id}, {$push: {"transactions":
+        {"_id": new mongoose.Types.ObjectId(),
+            "transactionName": req.params.transaction_name,
+            "transactionDate": Date.now()}}},
+        {new: true},
+        function(err, newTR) {
+            if (err) return handleError(err);
+
+            res.send(201, newTR);
+        });
+    next();
+});
+
 // Get all clients in the system
 server.get('/clients', function (req, res, next) {
     console.log('>>>' + server.url + '/clients: recieved GET request')
